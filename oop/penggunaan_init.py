38094e21fb38748e681b7d2e2befe3b71507a14a
Muhammad Nuh <muhammadnuh624@gmail.com>
2024-03-10 21:13:33 +0700

    2nd commit

--- oop/penggunaan_init.py
+++ oop/penggunaan_init.py
@@ -1,9 +1,12 @@
-class Orang:
+class orang:
     def __init__(self, nama):
         self.nama = nama
 
-    def katakanHai(self):
-        print(f"hai nama saya {self.nama}, apakabar masbro!!!")
-
-org = Orang("indah")
-org.katakanHai()
+    def nyapa(self):
+        print('halou masbro, %s ' % self.nama)
+        print('halou masbro, ' + self.nama)
+        print(f"Halou masbro {self.nama}")
+    
+# manggil object^diatas
+org = orang('kapibara')
+org.nyapa()
